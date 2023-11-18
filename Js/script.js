b249b8baf02389233e722c355cafda32ad7a2450
Divine-Praise <divineatansi123@gmail.com>
2023-11-17 22:10:24 -0800

    First Commit

---
+++ Js/script.js
@@ -0,0 +1,93 @@
+//onscroll
+document.addEventListener('scroll', () =>{
+    const nav = document.querySelector('.nav');
+    
+    if (window.scrollY > 0){
+        nav.classList.add('scrolled');
+    }else{
+        nav.classList.remove('scrolled');
+    }
+});
+
+//sroll padding
+const navigation = document.querySelector('.nav-link');
+const navigationHeight = navigation.offsetHeight;
+document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight + "px"
+);
+
+//testimonial
+var btn = document.getElementsByClassName("btnn");
+var slide = document.getElementById("slide");
+
+btn[0].onclick = function(){
+    slide.style.transform = "translateX(0px)";
+    for(i=0; i<4; i++){
+        btn[i].classList.remove("active");
+        this.classList.add("active")
+    }
+}
+btn[1].onclick = function(){
+    slide.style.transform = "translateX(-800px)";
+    for(i=0; i<4; i++){
+        btn[i].classList.remove("active");
+        this.classList.add("active")
+    }
+}
+btn[2].onclick = function(){
+    slide.style.transform = "translateX(-1600px)";
+    for(i=0; i<4; i++){
+        btn[i].classList.remove("active");
+        this.classList.add("active")
+    }
+}
+btn[3].onclick = function(){
+    slide.style.transform = "translateX(-2400px)";
+    for(i=0; i<4; i++){
+        btn[i].classList.remove("active");
+        this.classList.add("active")
+    }
+}
+
+//question
+const icon = document.querySelector('.dropdown-icon');
+const icon1 = document.querySelector('.dropdown-icon1');
+const icon2 = document.querySelector('.dropdown-icon2');
+const icon3 = document.querySelector('.dropdown-icon3');
+
+const spann = document.getElementById('dropdown');
+const spann1 = document.getElementById('dropdown1');
+const spann2 = document.getElementById('dropdown2');
+const spann3 = document.getElementById('dropdown3');
+
+
+icon.addEventListener('click', function(){
+    spann.classList.toggle('active2')
+    // spann.style.transition = "all ease-in-out .5s";
+    spann1.classList.remove('active2')
+    spann2.classList.remove('active2')
+    spann3.classList.remove('active2')
+})
+icon1.addEventListener('click', function(){
+    spann1.classList.toggle('active2')
+    spann.classList.remove('active2')
+    spann2.classList.remove('active2')
+    spann3.classList.remove('active2')
+})
+icon2.addEventListener('click', function(){
+    spann2.classList.toggle('active2')
+    spann.classList.remove('active2')
+    spann1.classList.remove('active2')
+    spann3.classList.remove('active2')
+})
+icon3.addEventListener('click', function(){
+    spann3.classList.toggle('active2')
+    spann.classList.remove('active2')
+    spann1.classList.remove('active2')
+    spann2.classList.remove('active2')
+});
+
+// update footer year
+var x = new Date()
+document.querySelector("#get-year").innerText = x.getFullYear();
